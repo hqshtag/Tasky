ac790e805d431e44d7062b86a854193f192c3e36
Wajih Tagourty <wajih.tagourty@gmail.com>
2020-06-06 13:22:23 +0100

    finished home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Pic from "../assets/img/1.png";
 
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <div className="home-container">
+      <div className="welcome-message">
+        <h1>Welcome Stranger</h1>
+        <h2>Any tasks for today?</h2>
+      </div>
+      <img src={Pic}></img>
+    </div>
+  );
 };
 
 export default Home;
