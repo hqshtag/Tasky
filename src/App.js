98ac2210448654b0056238446cf341495cb05090
Wajih Tagourty <wajih.tagourty@gmail.com>
2020-06-06 12:16:06 +0100

    updated dependencies/ bug fix/ moved imgs folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+import Home from "./components/Home";
+import Navbar from "./components/layout/Navbar";
+
+import "./assets/styles/stylesheet.scss";
+
 function App() {
-  return null;
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
 }
 
 export default App;
